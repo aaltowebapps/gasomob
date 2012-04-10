568e0c78f44665e6b352d92b1f1b014346fa82fd
Markus Ahonen <markus@kilke.net>
2012-04-10 21:33:49 +0300

    Modified production start script

--- gaso/gaso.sh
+++ gaso/gaso.sh
@@ -1,1 +1,7 @@
-NODE_ENV=production coffee app
+#!/bin/bash
+NODE_ENV=production
+echo Kill previous process...
+sudo killall -u gaso node
+echo Start gaso app...
+sudo -u gaso nohup coffee app &
+exit 0
