23170d37676de004b231b3fbe370c0b99f881ad7
Markus Ahonen <markus@kilke.net>
2012-05-07 16:33:11 +0300

    initial try for phonegap

---
+++ phonegap/js/mobileinit.js
@@ -0,0 +1,1 @@
+(function(){var a;a=$(document),a.bind("mobileinit",function(a){return _.extend($.mobile,{ajaxEnabled:!1,linkBindingEnabled:!1,hashListeningEnabled:!1,pushStateEnabled:!1})})}).call(this)
