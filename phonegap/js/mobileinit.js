f5a2983254901f5dd99240237e4416e23f814d52
Markus Ahonen <markus@kilke.net>
2012-05-11 15:06:37 +0300

    Removed phonegap stuff from bothering us at master branch, those are in phonegap-branch now

--- phonegap/js/mobileinit.js
+++
@@ -1,1 +0,0 @@
-(function(){var a;a=$(document),a.bind("mobileinit",function(a){return _.extend($.mobile,{ajaxEnabled:!1,linkBindingEnabled:!1,hashListeningEnabled:!1,pushStateEnabled:!1})})}).call(this)
